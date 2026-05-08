9049f1750bdcd2327c8fde91afd1582ff71f496a
gayashvihangana <gayashvihangana@gmail.com>
2026-05-08 09:14:36 -0400

    Add followers count

---
+++ followersCount/followers_count.js
@@ -0,0 +1,25 @@
+let count = 0;
+
+function increaseCount() {
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount() {
+    document.getElementById("countDisplay").innerText = count;
+}
+
+function checkCountValue() {
+    if (count === 10) {
+      alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+      alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+  }
+  
+function resetCount() {
+    count = 0;
+    displayCount();
+    alert("Followers count has been reset!");
+}
